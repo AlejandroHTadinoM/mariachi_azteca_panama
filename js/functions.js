d9c4453d64cb19a3a40db397a0eb2dc7f7f5c508
alejandrohtadinom <alejandrohtadinom@gmail.com>
2017-03-07 13:25:45 -0400

    Completed content, added img files

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,22 @@
-$(document).ready(function () {
-    console.log("loaded");
-})
+$(window).scroll(function () {
+	var wScroll = $(this).scrollTop();
+
+	if (wScroll > $('.navbar').height()) {
+
+		$('#brand-logo').animate({
+            width: "64px"
+        }, 300);
+
+    } else {
+        $('#brand-logo').animate({
+            width: "150px"
+        }, 300);
+    };
+
+    if (wScroll > $('.section__about').offset().top) {
+        $('figure > img').animate({
+            opacity: 1
+        }, 300);
+    };
+
+});
